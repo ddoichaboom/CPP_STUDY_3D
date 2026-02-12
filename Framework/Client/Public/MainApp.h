a419bcec5d44822c8de3d3707df0537759e12481
ddoichaboom <chahojun1004@gmail.com>
2026-02-12 12:47:12 +0900

    2일차 수정 - Engine 프로젝트 생성 및 추가 - Base 코드 작성 ( 레퍼런스 카운트 개념 ) - CMainApp 선언부 틀 작성

--- Framework/Client/Public/MainApp.h
+++ Framework/Client/Public/MainApp.h
@@ -1,7 +1,19 @@
 #pragma once
 
-class CMainApp
-{
+#include "Client_Defines.h"
 
+class CMainApp final : public CBase
+{
+private:
+	CMainApp();
+	virtual ~CMainApp();
+
+public:
+	HRESULT Initialize();
+
+	
+public:
+	static CMainApp* Create();
+	virtual void Free() override;
 };
 
---
+++ Framework/Engine/Public/Base.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Engine_Defines.h"
+
+class CBase	abstract
+{
+protected:
+	CBase();
+	virtual ~CBase() = default;
+
+public:
+	// 레퍼런스 카운트를 증가시킨다.
+	_uint AddRef();
+
+	// 레퍼런스 카운트를 감소시킨다. or 삭제한다 
+	_uint Release();
+
+protected:
+	_uint			m_iRefCnt = {};		// 0으로 유니폼 초기화
+
+public:
+	virtual void Free();
+
+};
+
