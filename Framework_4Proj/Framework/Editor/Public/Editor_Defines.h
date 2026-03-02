078fa8cb614188d87d722aca18a4a7896f8c6bdf
CHA\chaho <chahojun1004@gmail.com>
2026-03-02 21:10:17 +0900

    Editor 프로젝트 ImGui 적용 진행

---
+++ Framework_4Proj/Framework/Editor/Public/Editor_Defines.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace Editor 
+{
+	static const unsigned int     g_iWinSizeX = { 1600 };  
+	static const unsigned int     g_iWinSizeY = { 900 };
+
+}
+
+extern HWND   g_hWnd;
+
+using namespace Editor;
